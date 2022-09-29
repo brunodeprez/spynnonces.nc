be339100c4bbf4d97a088e8e57af65b237b42bd1
Bruno Deprez <brunod@spc.int>
2022-09-30 10:47:46 +1100

    Put config in sperate files

--- annonces.py
+++ annonces.py
@@ -4,36 +4,18 @@ import urllib
 from tinydb import TinyDB, where
 from mailjet_rest import Client
 
-MAILJET_API_KEY = ''
-MAILJET_API_SECRET = ''
-
 categories_to_remove = {
     "automobiles.nc": ["Divers", "Pièces moteurs", "Carrosseries", "Éclairages"],
     "2roues.nc": ["Pièces détachées Moto"]
 }    
 
-config = [{
-    "email": "bruno.deprez@gmail.com",
-    "searches": [
-        {
-            "id":1,
-            "site":"automobiles.nc",
-            "keywords": "berlingo",
-            "max_km": 60000,
-            "min_price": 800000
-        },
-        {
-            "id":2,
-            "site":"2roues.nc",
-            "keywords": "v strom",
-        }
-    ]
-}]
+with open('config.json', 'r') as f:
+  config = json.load(f)
+
+with open('mail-config.json', 'r') as f:
+  mail_config = json.load(f)
 
 def filter_hit():
-    # print(hit['kind'])
-    # print(hit['category']['root_name'])
-    # print(hit['category']['name'])
     if hit['kind'] != "sell" or  hit['category']['root_name'] != search['site'] or hit['category']['name'] in categories_to_remove[search['site']]:
         return False
     if search.get('max_km') and hit['custom_fields'].get('km') and hit['custom_fields']['km'] > search['max_km']:
@@ -42,16 +24,13 @@ def filter_hit():
     return True
 
 def send_email():
-    api_key = ''
-    api_secret = ''
+    api_key = mail_config['MailJet']['Api_Key']
+    api_secret = mail_config['MailJet']['Api_Secret']
     mailjet = Client(auth=(api_key, api_secret), version='v3.1')
     data = {
     'Messages': [
         {
-        "From": {
-            "Email": "bruno.deprez@gmail.com",
-            "Name": "Bruno"
-        },
+        "From": mail_config['MailJet']['From'],
         "To": [
             {
             "Email": x['email']
@@ -63,9 +42,7 @@ def send_email():
         }
     ]
     }
-    #result = mailjet.send.create(data=data)
-    #print (result.status_code)
-    #print (result.json())
+    result = mailjet.send.create(data=data)
 
 def process_new_hit():
     processedAdsTable.insert({'search_id': search['id'] , 'hit_id': hit['id']})
